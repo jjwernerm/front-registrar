bb56c20b4e7e2e87f998cdb1e0660fcff649d55d
jjwernerm <joannywerner@gmail.com>
2024-08-26 03:14:48 -0400

    commit

--- src/views/Registrar.jsx
+++ src/views/Registrar.jsx
@@ -72,7 +72,7 @@ export default function Registrar() {
       });
 
       // Resetea los campos del formulario
-      setIdProducto(''); 
+      setIdProducto('');
       setNombre('');
 
     } catch (error) {
@@ -84,88 +84,94 @@ export default function Registrar() {
 
     } finally {
       // Limpia el mensaje global y habilita el botón nuevamente después de 3 segundos
-      await new Promise(resolve => setTimeout(resolve, 3000)); 
+      await new Promise(resolve => setTimeout(resolve, 3000));
       setGlobalErrorMsg('');
-      setBtn(false); 
+      setBtn(false);
     };
 
   };
 
   // Renderización del formulario
   return (
-    <div className="flex justify-center my-4">
-      <form
-        className="border border-gray-100 rounded-xl w-9/12 md:w-3/12 p-4 shadow-md"
-        onSubmit={handleSubmit} // Asignar la función para manejar el envío del formulario
-      >
-        <div className="text-center font-bold">Registrar Producto</div>
-
-        {/* Campo Id Producto */}
-        <div className="mt-4">
-          <label htmlFor="input-id" className="text-gray-500">Id Producto <span className='text-red-500'>*</span></label>
-          <input
-            type="number"
-            placeholder="Escribe el id del producto"
-            id="input-id"
-            className={`${errorIdProducto ? 'border-red-500 ring-red-500' : 'border-gray-100 focus:ring-green-400'} bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1`} // Estilos condicionales según haya error o no
-            value={idproducto} // Valor del input vinculado al estado
-            onChange={handleIdProductoChange} // Manejador de cambio para el campo
-          />
-          
-          {/* Verifica si `errorIdProducto` es true, lo que indica que hay un error en el campo Id Producto */}
-          {errorIdProducto && (
-            // Si hay un error, muestra un mensaje de error dentro de un `div`
-            <div className="text-red-500 text-xs mt-1">
-              El campo Id Producto es obligatorio
-            </div>
-          )}
-
-        </div>
-
-        {/* Campo Nombre del Producto */}
-        <div className="mt-4">
-          <label htmlFor="input-nombre" className="text-gray-500">Nombre del Producto <span className='text-red-500'>*</span></label>
-          <input
-            type="text"
-            placeholder="Escribe el nombre del producto"
-            id="input-nombre"
-            className={`${errorNombre ? 'border-red-500 ring-red-500' : 'border-gray-100 focus:ring-green-400'} bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1`} // Estilos condicionales según haya error o no
-            value={nombre} // Valor del input vinculado al estado
-            onChange={handleNombreChange} // Manejador de cambio para el campo
-          />
-          
-          {/* Verifica si `errorNombre` es true, lo que indica que hay un error en el campo Nombre del Producto */}
-          {errorNombre && (
-            // Si hay un error, muestra un mensaje de error dentro de un `div`
-            <div className="text-red-500 text-xs mt-1">
-              El campo Nombre del Producto es obligatorio
-            </div>
-          )}
-          
-        </div>
-
-        {/* Botón de registro */}
-        <button
-          type="submit"
-          className="bg-emerald-600 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 
-          hover:bg-green-400 flex items-center justify-center"
-          disabled={btn} // Deshabilita el botón si el estado btn es true
+    <>
+      <a
+        href='https://joannywerner.com/dashboardmern'
+        className="ml-5 underline">
+        Regresar al Dashboard        
+      </a>
+      <div className="flex justify-center my-4">
+        <form
+          className="border border-gray-100 rounded-xl w-9/12 md:w-3/12 p-4 shadow-md"
+          onSubmit={handleSubmit} // Asignar la función para manejar el envío del formulario
         >
-          {spinner ? ( // Si el spinner es true, muestra el icono de carga
-            <div className="flex items-center">
-              <ImSpinner9 className="animate-spin h-5 w-5 text-white mr-2" />
-              Registrando...
-            </div>
-          ) : (
-            'Registrar' // Si no, muestra el texto "Registrar"
-          )}
-        </button>
+          <div className="text-center font-bold">Registrar Producto</div>
 
-        {/* Muestra la alerta si existe un mensaje global */}
-        {globalErrorMsg.msg && <Alert props={globalErrorMsg} />}
+          {/* Campo Id Producto */}
+          <div className="mt-4">
+            <label htmlFor="input-id" className="text-gray-500">Id Producto <span className='text-red-500'>*</span></label>
+            <input
+              type="number"
+              placeholder="Escribe el id del producto"
+              id="input-id"
+              className={`${errorIdProducto ? 'border-red-500 ring-red-500' : 'border-gray-100 focus:ring-green-400'} bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1`} // Estilos condicionales según haya error o no
+              value={idproducto} // Valor del input vinculado al estado
+              onChange={handleIdProductoChange} // Manejador de cambio para el campo
+            />
 
-      </form>
-    </div>
+            {/* Verifica si `errorIdProducto` es true, lo que indica que hay un error en el campo Id Producto */}
+            {errorIdProducto && (
+              // Si hay un error, muestra un mensaje de error dentro de un `div`
+              <div className="text-red-500 text-xs mt-1">
+                El campo Id Producto es obligatorio
+              </div>
+            )}
 
+          </div>
+
+          {/* Campo Nombre del Producto */}
+          <div className="mt-4">
+            <label htmlFor="input-nombre" className="text-gray-500">Nombre del Producto <span className='text-red-500'>*</span></label>
+            <input
+              type="text"
+              placeholder="Escribe el nombre del producto"
+              id="input-nombre"
+              className={`${errorNombre ? 'border-red-500 ring-red-500' : 'border-gray-100 focus:ring-green-400'} bg-gray-100 p-1 rounded-xl w-full outline-none focus:ring-1`} // Estilos condicionales según haya error o no
+              value={nombre} // Valor del input vinculado al estado
+              onChange={handleNombreChange} // Manejador de cambio para el campo
+            />
+
+            {/* Verifica si `errorNombre` es true, lo que indica que hay un error en el campo Nombre del Producto */}
+            {errorNombre && (
+              // Si hay un error, muestra un mensaje de error dentro de un `div`
+              <div className="text-red-500 text-xs mt-1">
+                El campo Nombre del Producto es obligatorio
+              </div>
+            )}
+
+          </div>
+
+          {/* Botón de registro */}
+          <button
+            type="submit"
+            className="bg-emerald-600 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 
+          hover:bg-green-400 flex items-center justify-center"
+            disabled={btn} // Deshabilita el botón si el estado btn es true
+          >
+            {spinner ? ( // Si el spinner es true, muestra el icono de carga
+              <div className="flex items-center">
+                <ImSpinner9 className="animate-spin h-5 w-5 text-white mr-2" />
+                Registrando...
+              </div>
+            ) : (
+              'Registrar' // Si no, muestra el texto "Registrar"
+            )}
+          </button>
+
+          {/* Muestra la alerta si existe un mensaje global */}
+          {globalErrorMsg.msg && <Alert props={globalErrorMsg} />}
+
+        </form>
+      </div>
+    </>
   );
 };
