6db16ba6df734b6b3bac121a4528fb97558fca72
jjwernerm <joannywerner@gmail.com>
2024-08-26 14:34:08 -0400

    dashboard

--- src/views/Registrar.jsx
+++ src/views/Registrar.jsx
@@ -95,7 +95,7 @@ export default function Registrar() {
   return (
     <>
       <a
-        href='https://joannywerner.com/dashboardmern'
+        href='https://crud-producto-seven.vercel.app/'
         className="ml-5 underline">
         Regresar al Dashboard        
       </a>
